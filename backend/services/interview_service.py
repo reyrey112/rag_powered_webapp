329f2de05bbf0ecc9060dbb03c785498babe10c0
Reyden Davies <reydencdavies@gmail.com>
2026-07-11 10:17:16 -0400

    services not used

--- backend/services/interview_service.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-backend_root = os.path.abspath(os.path.join(current_dir, ".."))
-if backend_root not in sys.path:
-    sys.path.append(backend_root)
-
-# from backend.legacy.interview_state import 
